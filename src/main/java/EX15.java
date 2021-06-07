9aab8ddc8fd0ab1386e67701023c2b4db30931f6
Vincent10z <84353877+Vincent10z@users.noreply.github.com>
2021-06-06 20:36:41 -0400

    Initial commit

---
+++ src/main/java/EX15.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Vincent Smith
+ */
+import java.util.Scanner;
+public class EX15 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the password? ");
+        String pass = input.next();
+
+        if (Valid(password)) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("I don't know you ");
+
+        }
+
+
+    }
+
+public static boolean Valid(String password){
+
+        if (password.charAt())
+
+
+}
+}
